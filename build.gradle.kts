c0df43b91800fa118004f7145ab84290e17c2216
arthur.klezovich <klezovich@gmail.com>
2021-12-02 12:10:23 +0100

    Generate XML

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,4 +49,7 @@ tasks.test {
 }
 tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(true)
+    }
 }
